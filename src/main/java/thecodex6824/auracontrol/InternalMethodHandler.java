dfd54544bf2b32fb57872b7d9682432167ce5644
TheCodex6824 <thecodex6824@gmail.com>
2020-07-18 16:45:14 -0400

    When you forget to change the license headers that you copied from your other addon

--- src/main/java/thecodex6824/auracontrol/InternalMethodHandler.java
+++ src/main/java/thecodex6824/auracontrol/InternalMethodHandler.java
@@ -1,21 +1,21 @@
 /**
- *  Thaumic Augmentation
- *  Copyright (c) 2019 TheCodex6824.
+ *  AuraControl
+ *  Copyright (c) 2020 TheCodex6824.
  *
- *  This file is part of Thaumic Augmentation.
+ *  This file is part of AuraControl.
  *
- *  Thaumic Augmentation is free software: you can redistribute it and/or modify
+ *  AuraControl is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Thaumic Augmentation is distributed in the hope that it will be useful,
+ *  AuraControl is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
  *
  *  You should have received a copy of the GNU Lesser General Public License
- *  along with Thaumic Augmentation.  If not, see <https://www.gnu.org/licenses/>.
+ *  along with AuraControl.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package thecodex6824.auracontrol;
